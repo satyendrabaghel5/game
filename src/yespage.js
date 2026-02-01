79373c5d83b2a385f2bf86544135f407c5dee27b
Satyendra Kumar Baghel <165415490+satyendrabaghel5@users.noreply.github.com>
2026-02-01 17:06:46 +0530

    Update yespage.js

--- src/yespage.js
+++ src/yespage.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./App.css";
 
+const icons = ["💖", "💋", "💕", "💓"];
+
 function YesPage() {
     return (
         <div className="container">
+            {/* ❤️ Floating Hearts */}
+            <div className="floating-bg">
+                {Array.from({ length: 25 }).map((_, i) => (
+                    <span
+                        key={i}
+                        style={{
+                            left: `${Math.random() * 100}%`,
+                            animationDelay: `${Math.random() * 5}s`,
+                            animationDuration: `${6 + Math.random() * 6}s`,
+                            fontSize: `${18 + Math.random() * 30}px`,
+                        }}
+                    >
+                        {icons[Math.floor(Math.random() * icons.length)]}
+                    </span>
+                ))}
+            </div>
+
             <h1>Yayyy! 💖 You are my Valentine 🥰</h1>
 
             <p style={{ fontSize: "20px", textAlign: "center", marginTop: "15px" }}>
-                You just made my heart special 💕 <br />
+                You just made my heart so happy 💕 <br />
                 Let’s make this Valentine unforgettable ✨
             </p>
 
